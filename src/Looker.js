67dc97660f63e40d6055171e4f76a2d4d2d1d2df
Francesco Sullo <sullof@gmail.com>
2021-04-29 11:17:46 -0700

    Changes to avoid repetition

--- src/Looker.js
+++ src/Looker.js
@@ -1,12 +1,23 @@
 const superagent = require('superagent')
 const _ = require('lodash')
 const Sql = require('./Sql')
+const fs = require('fs-extra')
+const path = require('path')
+const JSONdb = require('simple-json-db')
+let db
 
 async function sleep(millis) {
   return new Promise(resolve => setTimeout(resolve, millis))
 }
 
 class Looker extends Sql {
+
+  constructor() {
+    super();
+    fs.ensureDirSync(process.env.JSONDB_DIR)
+    db = new JSONdb(path.resolve(process.env.JSONDB_DIR, 'data.json'))
+  }
+
 
   async insertCoin(data) {
 
@@ -39,52 +50,60 @@ class Looker extends Sql {
   }
 
   async getMarketCapData() {
-
+    const looptime = parseInt(process.env.LOOPTIME) * 1000
     while (true) {
-      try {
-        let res = await superagent
-            .get('https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest')
-            .query({
-              start: 1,
-              limit: process.env.NODE_ENV === 'production' ? 2500 : 5,
-              convert: 'USD'
-            })
-            .set('X-CMC_PRO_API_KEY', process.env.COINMARKETCAP_KEY)
-            .accept('application/json')
+      let lastGrabAt = db.get('lastGrabAt')
+      if (!lastGrabAt || Date.now() - lastGrabAt > looptime) {
+        try {
+          let res = await superagent
+              .get('https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest')
+              .query({
+                start: 1,
+                limit: process.env.LIMIT,
+                convert: 'USD'
+              })
+              .set('X-CMC_PRO_API_KEY', process.env.COINMARKETCAP_KEY)
+              .accept('application/json')
 
-        for (let data of res.body.data) {
+          for (let data of res.body.data) {
 
-          await this.insertCoin(data)
+            await this.insertCoin(data)
 
-          const {
-            id,
-            max_supply,
-            total_supply,
-            circulating_supply,
-            cmc_rank,
-            last_updated
-          } = data
+            const {
+              id,
+              max_supply,
+              total_supply,
+              circulating_supply,
+              cmc_rank,
+              last_updated
+            } = data
 
-          let values = {
-            id,
-            max_supply,
-            total_supply,
-            circulating_supply,
-            cmc_rank,
-            last_updated
+            let values = {
+              id,
+              max_supply,
+              total_supply,
+              circulating_supply,
+              cmc_rank,
+              last_updated
+            }
+
+            for (let key in data.quote.USD) {
+              values[`quote_USD_${key}`] = data.quote.USD[key]
+            }
+
+            await (await this.sql()).insert(values).into('coins_data')
           }
-
-          for (let key in data.quote.USD) {
-            values[`quote_USD_${key}`] = data.quote.USD[key]
-          }
-
-          await (await this.sql()).insert(values).into('coins_data')
+          db.set('lastGrabAt', Date.now())
+          console.log('Data saved at', res.body.status.timestamp)
+        } catch (e) {
+          console.log(e)
         }
-        console.log('Data saved at', res.body.status.timestamp)
-      } catch (e) {
-        console.log(e)
+      } else {
+        if (process.env.NODE_ENV === 'development') {
+          console.log('Skipping')
+        }
       }
-      await sleep(2 * 1000 * 3600)
+      await sleep(Math.round(looptime / 10))
     }
   }
 
