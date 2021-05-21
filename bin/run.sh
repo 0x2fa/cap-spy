233db94acdb508fcb055f8ab3394319131d53d3d
Francesco Sullo <sullof@gmail.com>
2021-05-21 15:26:08 -0700

    Adding cryptoexplorer

---
+++ bin/run.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+docker run -d \
+    --name spydb \
+    --restart unless-stopped \
+    -p 5432 \
+    -v $PGDATA_DIR:/var/lib/postgresql/data/pgdata \
+    -e PGDATA=/var/lib/postgresql/data/pgdata \
+    -e POSTGRES_PASSWORD=$POSTGRES_PASSWORD \
+    -e POSTGRES_USER=$POSTGRES_USER \
+    -e POSTGRES_DB=$POSTGRES_DB \
+    postgres:13
+
+docker run -d \
+    --name looker \
+    --restart unless-stopped \
+    -v $PWD:/usr/src/app \
+    -v $JSONDB_DIR:/usr/src/db \
+    -e JSONDB_DIR=/usr/src/db \
+    -e LIMIT=$LIMIT \
+    -e LOOPTIME=$LOOPTIME \
+    -e POSTGRES_PASSWORD=$POSTGRES_PASSWORD \
+    -e POSTGRES_USER=$POSTGRES_USER \
+    -e POSTGRES_DB=$POSTGRES_DB \
+    -e NODE_ENV=$NODE_ENV \
+    -e COINMARKETCAP_KEY=$COINMARKETCAP_KEY \
+    -e LOOKER_PASSWORD=$LOOKER_PASSWORD \
+    -w /usr/src/app \
+    node:erbium-alpine3.10 npm run looker
+
+docker run -d \
+  --name crypto-explorer \
+  -p 8501 \
+  --restart unless-stopped \
+  -e NODE_ENV=production \
+  -e VIRTUAL_HOST=brokenjazz.cc,www.brokenjazz.cc \
+  -e LETSENCRYPT_HOST=brokenjazz.cc,www.brokenjazz.cc \
+  -e LETSENCRYPT_EMAIL=brokenjazz@sullo.co \
+  -w /usr/src/app node:12.20.0-alpine3.10 npm run start
+  -e PG_CONF=$PG_CONF
+  -e VIRTUAL_HOST=$VIRTUAL_HOST
+  -e LETSENCRYPT_HOST=$LETSENCRYPT_HOST
+  -e LETSENCRYPT_EMAIL=$LETSENCRYPT_EMAIL
+  crypto-explorer
+
