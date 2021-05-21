afcded2eaaa7ae2e451532f004810da7f058eea0
Francesco Sullo <sullof@gmail.com>
2021-05-21 16:05:59 -0700

    Adding alert bot 8

--- bin/run-explorer.sh
+++ bin/run-explorer.sh
@@ -4,6 +4,7 @@ docker run -d \
     --name explorer \
     --restart unless-stopped \
     -p 8501 \
+    --link spydb:postgres \
     -e PG_CONF="$PG_CONF" \
     -e VIRTUAL_HOST=$VIRTUAL_HOST \
     -e LETSENCRYPT_HOST=$LETSENCRYPT_HOST \
